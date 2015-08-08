9b64770af5f65464a80e913209fc84ca148175ea
Gökberk Yaltıraklı <webdosusb@gmail.com>
2015-08-08 17:45:13 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name = "hackchat",
+    py_modules = ["hackchat"],
+    version = "0.1.0",
+    description = "An event-driven hack.chat client library.",
+    author = "Gokberk Yaltirakli",
+    author_email = "webdosusb@gmail.com",
+    url = "https://github.com/gkbrk/hackchat",
+    keywords = ["hack.chat", "event", "client", "chat"]
+)
