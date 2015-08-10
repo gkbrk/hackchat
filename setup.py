5205aea0d9840a75b0f0526465e344e7f45d9680
Gökberk Yaltıraklı <webdosusb@gmail.com>
2015-08-10 23:01:27 +0300

    Updated PyPI package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = "hackchat",
     py_modules = ["hackchat"],
-    version = "0.1.1",
+    version = "0.1.2",
     description = "An event-driven hack.chat client library.",
     author = "Gokberk Yaltirakli",
     author_email = "webdosusb@gmail.com",
