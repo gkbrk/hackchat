f8c96dc1ce528ba7800130e43848127f0db3e057
Gökberk Yaltıraklı <webdosusb@gmail.com>
2017-06-04 13:50:41 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "hackchat",
     py_modules = ["hackchat"],
-    version = "0.1.6",
+    version = "0.2.0",
     description = "An event-driven hack.chat client library.",
     author = "Gokberk Yaltirakli",
     author_email = "webdosusb@gmail.com",
