04ca32fa2220d937276ed6e5e1572480ff695abb
Gokberk Yaltirakli <git@gkbrk.com>
2023-06-25 23:38:07 +0300

    Bumped version to 0.3.0 and pushed to PyPI

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "hackchat",
     py_modules = ["hackchat"],
-    version = "0.2.0",
+    version = "0.3.0",
     description = "An event-driven hack.chat client library.",
     author = "Gokberk Yaltirakli",
     author_email = "webdosusb@gmail.com",
